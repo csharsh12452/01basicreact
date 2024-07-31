2e44d503c7922c48686cc0c61629e916b0704b55
csharsh12452 <2021csharsh12452@poornima.edu.in>
2024-07-31 22:41:10 +0530

    react js

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+ const userName='Harsh Bajpai'
+
+  return (
+    <>
+    <Chai/>
+    <h1>bjP {userName}</h1>
+    <p>hrah</p>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h1>Harsh supports bjp</h1>
+    )
+}
+
+export default Chai;
